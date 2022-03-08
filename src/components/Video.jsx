6ec38abb852cad11c9997e8a2572d2c0a597bd1b
Oguz <yilmazoguz@outlook.com>
2022-03-08 16:31:53 +0300

    Build rough architecture of Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import video from '../data/video';
 
-function Video() {
+function Video({id, title, embedUrl, views, createdAt, upvotes, downvotes, comments}) {
   return (
     <div>
-      
+      <h1 className='title'>{video.title}</h1>
+      <div>
+        <span>{views} | </span>
+        <span>{createdAt}</span>
+      </div>
+      <div>
+        <button>{upvotes}</button>
+        <button>{downvotes}</button>
+      </div>
+      <button>Hide Comments</button>
+      <p>{comments.id}</p>
+      <p>{comments.user}</p>
+      <h2>{comments.comment}</h2>
     </div>
   )
 }
